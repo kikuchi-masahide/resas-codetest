8b3f150a4c5dc789fc189bee521de5389261d2f6
菊池雅英 <m20000406k@gmail.com>
2023-12-19 16:26:00 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import { sum } from "./sum";
+
+const comp = computed(() => sum(1, 2));
+</script>
+
+<template>1+2={{ comp }}</template>
+
+<style scoped></style>
