af5bc7fc7dd68dc85e89cf2805fe4ad0a6b098b9
菊池雅英 <m20000406k@gmail.com>
2023-12-20 22:05:39 +0900

    feat: eslint/prettier導入
    fix: delete .eslintrc.cjs

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,5 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
 createApp(App).use(createPinia()).mount("#app");
