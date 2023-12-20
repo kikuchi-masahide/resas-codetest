6c66908d37f86626317a49e30ccbc1bf404922a7
菊池雅英 <m20000406k@gmail.com>
2023-12-20 09:31:00 +0900

    feat: pinia導入

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import { sum } from "./sum";
+import { useStore } from "./store/store";
 
 const comp = computed(() => sum(1, 2));
+const store = useStore();
 </script>
 
-<template>1+2={{ comp }}</template>
+<template>
+    1+2={{ comp }}<br />
+    store.count={{ store.count }}<br />
+    <button @click="store.increment">increment</button>
+</template>
 
 <style scoped></style>
---
+++ src/store/store.ts
@@ -0,0 +1,13 @@
+import { defineStore } from "pinia";
+
+export const useStore = defineStore({
+    id: "main",
+    state: () => ({
+        count: 0,
+    }),
+    actions: {
+        increment() {
+            this.count++;
+        },
+    },
+});
